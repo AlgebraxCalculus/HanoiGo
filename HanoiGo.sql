a05b8ad26bd5a1cdc7d79c06012cf87a0b1eff8d
CG - [weeb] <130551183+Do-Trung-Quan@users.noreply.github.com>
2025-09-24 13:23:34 +0700

    Update HanoiGo.sql

--- HanoiGo.sql
+++ HanoiGo.sql
@@ -21,14 +21,19 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 -- =========================
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) Unique NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
+	firebase_uid varchar(255) unique,
+	profile_picture(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     last_login TIMESTAMP,
     points INT DEFAULT 0,
     rank INT
 );
+
+select * from users
+
 
 -- =========================
 -- Categories
