3694f1e1cd12f2ee4270b1045a1f205e4b2426b4
AlgebraxCalculus <112nh.thuytranthanh.c3chuvanan@gmail.com>
2025-10-07 12:39:46 +0700

    update config

--- MyApplication/app/build.gradle.kts
+++ MyApplication/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
-//    alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -33,26 +33,34 @@ android {
 }
 
 dependencies {
-
+    // AndroidX core UI
     implementation(libs.appcompat)
-    implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.google.android.material:material:1.12.0")
 
+    // Firebase
     implementation(libs.firebase.auth)
+
+    // Google Identity & Authentication
     implementation(libs.credentials)
     implementation(libs.credentials.play.services.auth)
     implementation(libs.googleid)
+    implementation("com.google.android.gms:play-services-auth:21.4.0")
 
+    // Networking & Image
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.github.bumptech.glide:glide:5.0.5")
+
+    // Location & Map
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.mapbox.mapboxsdk:mapbox-android-sdk:9.7.2")
+
+    // UI components
+    implementation("io.github.chaosleung:pinview:1.4.4")
+
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.google.android.material:material:1.12.0")
-    implementation(libs.material)
-    implementation("com.github.bumptech.glide:glide:5.0.5")
-    implementation("com.google.android.gms:play-services-auth:21.4.0")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("io.github.chaosleung:pinview:1.4.4")
-    implementation("com.mapbox.mapboxsdk:mapbox-android-sdk:9.7.2")
-    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
